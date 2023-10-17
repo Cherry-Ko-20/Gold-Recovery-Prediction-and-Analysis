ae9c7e2521e3a839ed0db3d4d7223608712a469e
Cherry-Ko-20 <echory20@gmail.com>
2023-10-17 12:41:28 -0700

    adding libries and reading csv files

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pandas as pd
-import numpy as np 
+import numpy as np
+from sklearn.metrics import mean_absolute_error 
 
 train= pd.read_csv('files/gold_recovery_train.csv')
-source  = pd.read_csv('files/gold_recovery_full.csv')
-test = pd.read_csv('files/gold_recovary_test.csv')
+source=pd.read_csv('files/gold_recovery_full.csv')
+test=pd.read_csv('files/gold_recovery_test.csv')
 print(train)
 print(source)
 print(test)
