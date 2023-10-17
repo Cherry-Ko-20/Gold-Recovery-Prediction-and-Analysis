8d2bd7f978560888d304f800dd00de08d9b16b62
Cherry-Ko-20 <echory20@gmail.com>
2023-10-17 12:31:22 -0700

    read the files and import libries

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np 
+
+train= pd.read_csv('files/gold_recovery_train.csv')
+source  = pd.read_csv('files/gold_recovery_full.csv')
+test = pd.read_csv('files/gold_recovary_test.csv')
+print(train)
+print(source)
+print(test)
