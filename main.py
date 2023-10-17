15dca2e4ae9c8a532b4b91b536bf167ff555edf8
Cherry-Ko-20 <echory20@gmail.com>
2023-10-17 14:18:33 -0700

    fixed the code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_absolute_error 
+import matplotlib.pyplot as plt
 
 train= pd.read_csv('files/gold_recovery_train.csv')
 source=pd.read_csv('files/gold_recovery_full.csv')
@@ -10,9 +11,13 @@ print(source.info())
 print(test.info())
 
 
+#Check that recovery is calculated correctly 
+
+
 # Calculate recovery
 train['recovery_calc'] = (train['rougher.output.concentrate_au'] *
                                (train['rougher.input.feed_au'] - train['rougher.output.tail_au'])) / \
                               (train['rougher.input.feed_au'] *
                                (train['rougher.output.concentrate_au'] - train['rougher.output.tail_au'])) * 100
 print(train['recovery_calc'])
+
