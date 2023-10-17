0b83d5333ce254144a7503bfb21ddae2086da3dd
Cherry-Ko-20 <echory20@gmail.com>
2023-10-17 13:14:14 -0700

    calculating recovery values

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ test=pd.read_csv('files/gold_recovery_test.csv')
 print(train.info())
 print(source.info())
 print(test.info())
+
+
+# Calculate recovery
+train['recovery_calc'] = (train['rougher.output.concentrate_au'] *
+                               (train['rougher.input.feed_au'] - train['rougher.output.tail_au'])) / \
+                              (train['rougher.input.feed_au'] *
+                               (train['rougher.output.concentrate_au'] - train['rougher.output.tail_au'])) * 100
+print(train['recovery_calc'])
